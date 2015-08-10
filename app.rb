364cd60cec61ffa0d811beede7728eb5f913921b
colinjlacy <colin@webcake.co>
2015-08-10 16:55:20 -0500

    [ch2-static-template] sends a static template on the /static path

--- app.rb
+++ app.rb
@@ -4,3 +4,10 @@ require 'sinatra'
 get '/' do
 	"Responding with some basic text.  Like a boss."
 end
+
+#how to create a route
+
+get '/static/?' do
+	File.read(File.join('views','template.html'))
+end
+
