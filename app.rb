cb34722233d831524efc6b3b2096fc7965b55122
colinjlacy <colin@webcake.co>
2015-08-10 16:53:27 -0500

    [ch1-res-text] creates a route to respond with some basic text

--- app.rb
+++ app.rb
@@ -2,55 +2,5 @@
 require 'sinatra'
 
 get '/' do
-	File.read(File.join('views','template.html'))
-end
-
-#how to create a route
-
-get '/static/?' do	
-	File.read(File.join('views','template.html'))
-end
-
-#route parameter/parameter variable
-
-get '/params/:whatever/?' do
-	"You entered #{params['whatever']} as a route parameter"
-end
-
-get '/tts/:whatever/?' do
-	"TTS #{params['whatever']}"
-end
-#multiple params 
-	#localhost:4567/mulitple/chloe/params/frank
-
-get '/multiple/:this/params/:that/?' do
-	"I see <strong>#{params['this']}</strong> and <strong>#{params['that']}</strong> in your route params!"
-end
-
-#using a static erb template (embedded ruby file) do not need to run server.  
-	#looks like html file
-get '/erb/?' do
-	erb :template
-end
-
-#variables
-
-get '/erb/:thing/?' do
-	erb :variables, :locals => {:variable => params['thing']}
-end
-
-get '/vars/?' do
-	@variable = {
-		name: "Colin",
-		class: "TTS",
-		language: "Ruby"
-	}
-	erb :instance
-end
-
-
-#wildcard 
-
-get '*' do
-	status 404
+	"Responding with some basic text.  Like a boss."
 end
